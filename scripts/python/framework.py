0df317159243b29d714e8b28c55439bf6af49fd3
Joeri Hermans <joeri@joerihermans.com>
2014-08-03 00:49:42 +0200

    Add basic Python script for devices.

---
+++ scripts/python/framework.py
@@ -0,0 +1,73 @@
+#
+# IAS Basic device framework.
+#
+# Author: Joeri Hermans
+#
+
+import sys
+import socket
+import struct
+
+# Global members, which are required for the communication
+# with the remote IAS controller.
+gDeviceIdentifier = sys.argv[1]
+gControllerAddress = sys.argv[2]
+gControllerPort = int(sys.argv[3])
+gSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+gSocket.connect((gControllerAddress,gControllerPort))
+gRunning = True
+
+def updateState( stateIdentifier , newValue ):
+    global gSocket
+    stateIdentifierLength = len(stateIdentifier)
+    newValueLength = len(newValue)
+    data = struct.pack("!BBB",0x01,stateIdentifierLength,newValueLength);
+    data += str.encode(stateIdentifier)
+    data += str.encode(newValue)
+    gSocket.sendall(data)
+
+def authenticate():
+    global gDeviceIdentifier
+    global gSocket;
+    identifierLength = len(gDeviceIdentifier)
+    message = struct.pack("!BB",0x00,identifierLength) + bytes(gDeviceIdentifier.encode("ascii"));
+    gSocket.sendall(message);
+
+def processFeature(featureIdentifier,parameter):
+    # TODO Implement
+    print("Executing " + featureIdentifier + " with " + parameter)
+
+def processCommand():
+    global gSocket
+    global gRunning
+    data = gSocket.recv(3);
+    data = struct.unpack("!BBB",data)
+    if( data[0] != 0x01 ):
+        gRunning = False
+        return
+    featureIdentifierLength = data[1]
+    parameterLength = data[2]
+    featureIdentifier = gSocket.recv(featureIdentifierLength)
+    featureIdentifier = featureIdentifier.decode("ascii")
+    if( parameterLength > 0 ):
+        parameter = gSocket.recv(parameterLength)
+    else:
+        parameter = ""
+    parameter = parameter.decode("ascii")
+    processFeature(featureIdentifier,parameter)
+    
+
+def processCommands():
+    global gRunning
+    while( gRunning ):
+        try:
+            processCommand()
+        except:
+            gRunning = False
+
+def main():
+    authenticate()
+    processCommands()
+
+if( __name__ == "__main__" ):
+    main()
