796034100d0a38eeff0aa44ca261e251b25fb218
Joeri HERMANS <joeri@joerihermans.com>
2015-04-17 10:38:39 +0200

    Fix date, name and version formatting.

--- src/ias/threadpool/threadpool.cpp
+++ src/ias/threadpool/threadpool.cpp
@@ -2,8 +2,8 @@
  * A class which describes the actions and properties of a threadpool.
  *
  * @date                    Jul 2, 2014
- * @author                    Joeri HERMANS
- * @version                    0.1
+ * @author                  Joeri HERMANS
+ * @version                 0.1
  *
  * Copyright 2013 Joeri HERMANS
  *
