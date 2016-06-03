28e4c82877bf26e8fd1358341b99ab86d1a1f26a
Joeri Hermans <joeri@joerihermans.com>
2016-06-03 18:34:19 +0200

    Modify interfaces.

--- src/ias/technology/data_access/mysql_technology_data_access.h
+++ src/ias/technology/data_access/mysql_technology_data_access.h
@@ -1,6 +1,5 @@
 /**
- * A clas which provides an abstract data access layer for the
- * technology member.
+ * A class which represents the MySQL technology data access layer.
  *
  * @date                    03 06 2016
  * @author                  Joeri HERMANS
@@ -21,24 +20,42 @@
  * limitations under the License.
  */
 
+#ifdef IAS_DATABASE_DRIVER
+#if IAS_DATABASE_DRIVER == 'M' || IAS_DATABASE_DRIVER == 'A'
+
 // BEGIN Includes. ///////////////////////////////////////////////////
 
+// System dependencies.
+#include <mysql/mysql.h>
+#include <mysql/my_global.h>
+
 // Application dependencies.
-#include <ias/data_access/data_access.h>
-#include <ias/technology/technology.h>
+#include <ias/technology/data_access/feature_data_access.h>
+#include <ias/technology/data_access/member_data_access.h>
+#include <ias/technology/data_access/technology_data_access.h>
+#include <ias/technology/data_access/value_type_data_access.h>
 
 // END Includes. /////////////////////////////////////////////////////
 
-#ifndef IAS_TECHNOLOGY_DATA_ACCESS_H_
-#define IAS_TECHNOLOGY_DATA_ACCESS_H_
+#ifndef IAS_MYSQL_TECHNOLOGY_DATA_ACCESS_H_
+#define IAS_MYSQL_TECHNOLOGY_DATA_ACCESS_H_
 
 namespace ias {
 
-class technology_data_access : public data_access<ias::technology *> {
+class mysql_technology_data_access : public technology_data_access {
 
     public:
 
     // BEGIN Class constants. ////////////////////////////////////////////////
+
+    static const char kStmtAdd[];
+    static const char kStmtGetAll[];
+    static const char kStmtGetId[];
+    static const char kStmtRemove[];
+    static const char kStmtUpdate[];
+
+    static const std::size_t kDefaultStringSize = 80;
+
     // END Class constants. //////////////////////////////////////////////////
 
     private:
@@ -58,29 +75,29 @@ class technology_data_access : public data_access<ias::technology *> {
 
     // BEGIN Constructor. ////////////////////////////////////////////////////
 
-    technology_data_access(void) = default;
+    mysql_technology_data_access(ias::database_connection * dbConn);
 
     // END Constructor. //////////////////////////////////////////////////////
 
     // BEGIN Destructor. /////////////////////////////////////////////////////
 
-    virtual ~technology_data_access(void) = default;
+    virtual ~mysql_technology_data_access(void);
 
     // END Destructor. ///////////////////////////////////////////////////////
 
     // BEGIN Public methods. /////////////////////////////////////////////////
 
-    virtual std::vector<ias::technology *> get_all(void) = 0;
+    virtual std::vector<ias::technology *> get_all(void);
 
-    virtual ias::technology * get(const std::size_t id) = 0;
+    virtual ias::technology * get(const std::size_t id);
 
-    virtual void add(ias::technology * element) = 0;
+    virtual void add(ias::technology * element);
 
-    virtual void remove(ias::technology * element) = 0;
+    virtual void remove(ias::technology * element);
 
-    virtual void remove(const std::size_t id) = 0;
+    virtual void remove(const std::size_t id);
 
-    virtual void update(ias::technology * element) = 0;
+    virtual void update(ias::technology * element);
 
     // END Public methods. ///////////////////////////////////////////////////
 
@@ -91,4 +108,6 @@ class technology_data_access : public data_access<ias::technology *> {
 
 };
 
+#endif
+#endif
 #endif
